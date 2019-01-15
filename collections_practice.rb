16931792b2a220129111f335dd7336183955c7cd
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-15 14:01:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  new_array = array.dup
+  new_array[1] = array[2]
+  new_array[2] = array[1]
+  new_array
+end
+
+def swap_elements_from_to(array, x, y)
+  new_array = array.dup
+  new_array[x] = array[y]
+  new_array[y] = array[x]
+  new_array
+end
+
+# Advanced #2: Try writing test coverage for it!
 
